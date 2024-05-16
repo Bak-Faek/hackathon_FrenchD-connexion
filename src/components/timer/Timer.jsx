b7e497cc6ef65d61111bdc8f038e2fc5cb62be82
Séverine Dorimond Pereira <doris190485@hotmail.com>
2024-05-16 17:04:35 +0200

    adding background to pomodoro

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -7,105 +7,114 @@ import SettingsButton from '../settingsButton/SettingsButton';
 import { useContext, useState, useEffect, useRef } from 'react';
 import SettingsContext from '../../context/SettingsContext';
 
-
- const red = '#FF3131';
+const red = '#C38370';
 const green = '#00BF63';
- function Timer() {
-   const settingsInfo = useContext(SettingsContext);
 
-   const [isPaused, setIsPaused] = useState(true);
-   const [mode, setMode] = useState('work'); // work/break/null
-   const [secondsLeft, setSecondsLeft] = useState(0);
 
-   const secondsLeftRef = useRef(secondsLeft);
-   const isPausedRef = useRef(isPaused);
-   const modeRef = useRef(mode);
+function Timer() {
+  const settingsInfo = useContext(SettingsContext);
 
-   function tick() {
-     secondsLeftRef.current--;
-     setSecondsLeft(secondsLeftRef.current);
-   }
+  const [isPaused, setIsPaused] = useState(true);
+  const [mode, setMode] = useState('work'); // work/break/null
+  const [secondsLeft, setSecondsLeft] = useState(0);
 
-   useEffect(() => {
-     function switchMode() {
-       const nextMode = modeRef.current === 'work' ? 'break' : 'work';
-       const nextSeconds =
-         (nextMode === 'work'
-           ? settingsInfo.workMinutes
-           : settingsInfo.breakMinutes) * 60;
+  const secondsLeftRef = useRef(secondsLeft);
+  const isPausedRef = useRef(isPaused);
+  const modeRef = useRef(mode);
 
-       setMode(nextMode);
-       modeRef.current = nextMode;
+  function tick() {
+    secondsLeftRef.current--;
+    setSecondsLeft(secondsLeftRef.current);
+  }
 
-       setSecondsLeft(nextSeconds);
-       secondsLeftRef.current = nextSeconds;
-     }
 
-     secondsLeftRef.current = settingsInfo.workMinutes * 60;
-     setSecondsLeft(secondsLeftRef.current);
+  // function playBreakSound() {
+  //    const audio = new Audio('/public/Fluide.mp3');
+  //    audio.play();
+  // }
 
-     const interval = setInterval(() => {
-       if (isPausedRef.current) {
-         return;
-       }
-       if (secondsLeftRef.current === 0) {
-         return switchMode();
-       }
+  useEffect(() => {
+    function switchMode() {
+      const nextMode = modeRef.current === 'work' ? 'break' : 'work';
+      const nextSeconds =
+        (nextMode === 'work'
+          ? settingsInfo.workMinutes
+          : settingsInfo.breakMinutes) * 60;
 
-       tick();
-     }, 1000);
+      setMode(nextMode);
+      modeRef.current = nextMode;
 
-     return () => clearInterval(interval);
-   }, [settingsInfo]);
+      setSecondsLeft(nextSeconds);
+      secondsLeftRef.current = nextSeconds;
 
-   const totalSeconds =
-     mode === 'work'
-       ? settingsInfo.workMinutes * 60
-       : settingsInfo.breakMinutes * 60;
-   const percentage = Math.round((secondsLeft / totalSeconds) * 100);
+      
+    }
 
-   const minutes = Math.floor(secondsLeft / 60);
-   let seconds = secondsLeft % 60;
-   if (seconds < 10) seconds = '0' + seconds;
+    secondsLeftRef.current = settingsInfo.workMinutes * 60;
+    setSecondsLeft(secondsLeftRef.current);
 
-   return (
-     <>
-       <div className='progressbar-container'>
-         <div style={{ width: 200, height: 200 }}>
-           <CircularProgressbar
-             // counterClockwise={true}
-             value={percentage}
-             text={minutes + ':' + seconds}
-             styles={buildStyles({
-               textColor: '#fff',
-               pathColor: mode === 'work' ? red : green,
-               tailColor: 'rgba(255,255,255,.2)',
-             })}
-           />
-         </div>
-         <div className='button-container'>
-           {isPaused ? (
-             <PlayButton
-               onClick={() => {
-                 setIsPaused(false);
-                 isPausedRef.current = false;
-               }}
-             />
-           ) : (
-             <PauseButton
-               onClick={() => {
-                 setIsPaused(true);
-                 isPausedRef.current = true;
-               }}
-             />
-           )}
-         </div>
-         <div>
-           <SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
-         </div>
-       </div>
-     </>
-   );
- }
+    const interval = setInterval(() => {
+      if (isPausedRef.current) {
+        return;
+      }
+      if (secondsLeftRef.current === 0) {
+        return switchMode();
+      }
+
+      tick();
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, [settingsInfo]);
+
+  const totalSeconds =
+    mode === 'work'
+      ? settingsInfo.workMinutes * 60
+      : settingsInfo.breakMinutes * 60;
+  const percentage = Math.round((secondsLeft / totalSeconds) * 100);
+
+  const minutes = Math.floor(secondsLeft / 60);
+  let seconds = secondsLeft % 60;
+  if (seconds < 10) seconds = '0' + seconds;
+
+  return (
+    <>
+      <div className='progressbar-container'>
+        <div style={{ width: 200, height: 200 }}>
+          <CircularProgressbar
+            counterClockwise={true}
+            value={percentage}
+            text={minutes + ':' + seconds}
+            styles={buildStyles({
+              textColor: '#fff',
+              pathColor: mode === 'work' ? red : green,
+              tailColor: 'rgba(255,255,255,.2)',
+            })}
+          />
+        </div>
+        <div className='button-container'>
+          {isPaused ? (
+            <PlayButton
+              onClick={() => {
+                setIsPaused(false);
+                isPausedRef.current = false;
+              }}
+            />
+          ) : (
+            <PauseButton
+              onClick={() => {
+                setIsPaused(true);
+                isPausedRef.current = true;
+              }}
+            />
+          )}
+        </div>
+        <div>
+          <SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Timer;
--- src/pages/Pomodoro.jsx
+++ src/pages/Pomodoro.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react';
 import Settings from '../components/settings/Settings';
 import Timer from '../components/timer/Timer';
 import SettingsContext from '../context/SettingsContext';
+import './Pomodoro.css'
 
 function Pomodoro() {
   const [showSettings, setShowSettings] = useState(false);
   const [workMinutes, setWorkMinutes] = useState(45);
   const [breakMinutes, setBreakMinutes] = useState(15);
   return (
-    <div>
+    <div className='container-pomodoro'>
       <SettingsContext.Provider value={{showSettings, setShowSettings,workMinutes, breakMinutes, setWorkMinutes,setBreakMinutes}}>
         {showSettings ? <Settings /> : <Timer />}
       </SettingsContext.Provider>
