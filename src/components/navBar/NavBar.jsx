077705a38d8b542f1a2957155052405e8365a161
Pat <hoang.patrick@outlook.com>
2024-05-16 15:24:13 +0200

    ok

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -44,7 +44,6 @@ function ResponsiveAppBar() {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
